81a1c20ff0408ddd849a1e375304e7e22e3fb065
Kaiminy <104949057+Kaiminy@users.noreply.github.com>
2023-03-24 03:08:19 +0000

    tasks

---
+++ tasks.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "imageio.h"
+//task a
+
+int height, width;
+
+void invert(std::string input){
+  int j;
+  int k;
+  for (int row=0; row<j;row++){
+    out[row][col]=(255-1 img[row][col]);
+  }
+  return;
+  height=j;
+  width=k;
+}
+
+//task b
+void invert_half(int &height, int &width){
+  int originalheight=height;
+  int originalwidth= width;
+  for (int row=0;row<j;row++){
+    for (int k=0; col<k;k++)
+  }{
+    if (col>k /2){
+      copy[j][k]=255-image [j][k];
+    }
+    else{
+      copy image;
+    }
+  }
+}
+
+//task c
+void box(int j,k){
+  for (int row=0; row<j;row++){
+    out[row][col]=( img[row][col]);
+    255/2=out;
+
+    int &j=j/2;
+    int &k=k/2;
+    for (int col = int&j; int row= int&k;col++){
+      out[j][k];
+    }
+  
+}
+
+//task d frame
+
+void frame(int l,m){
+ for (int row=0; row<j;row++){
+    out[row][col]=( img[row][col]);
+   for (int col=m, int row=l);
+     if col ==l/4 || row == m/4;
+     out[l][m]=255;
+}
