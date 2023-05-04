840cef84c40ca87a7172903c0a1fe3bda20575a7
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-05-04 13:24:56 -0500

    add tasks.h

---
+++ tasks.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+void invert (std::string input);
+void invert_half(std::string input);
+void box(std::string input);
+void frame(std:: string input);
+void scale (std::string input);
