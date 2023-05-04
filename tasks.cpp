bcbf1009bc489da584681b9f2fc9889afa476883
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-05-04 14:29:36 -0500

    modified tasks

--- tasks.cpp
+++ tasks.cpp
@@ -1,56 +1,83 @@
 #include <iostream>
 #include "imageio.h"
+#include "tasks.h"
 //task a
 
 int height, width;
 
 void invert(std::string input){
+  int img[MAX_H][MAX_W];
   int j;
   int k;
+  readImage(input, img, j, k); 
+  int out[MAX_H][MAX_W];
   for (int row=0; row<j;row++){
-    out[row][col]=(255-1 img[row][col]);
+      for (int col = 0 ; col < k ; col++){
+    out[row][col]=255-img[row][col];
   }
-  return;
+}
+ /* return;
   height=j;
-  width=k;
+  width=k;*/
+  writeImage("taskA.pgm",out, j, k);
 }
 
 //task b
-void invert_half(int &height, int &width){
-  int originalheight=height;
-  int originalwidth= width;
+void invert_half(std::string input){
+  int img[MAX_H][MAX_W];
+  int j;
+  int k;
+  readImage(input, img, j, k); 
+  int out[MAX_H][MAX_W]; 
   for (int row=0;row<j;row++){
-    for (int k=0; col<k;k++)
-  }{
+    for (int col=0; col<k;col++)
+  {
     if (col>k /2){
-      copy[j][k]=255-image [j][k];
+      out[row][col]=255-img [row][col];
     }
     else{
-      copy image;
+      out[row][col] = img[row][col];
     }
   }
 }
+    writeImage("taskB.pgm",out, j, k);
+    }
+
+    
 
 //task c
-void box(int j,k){
-  for (int row=0; row<j;row++){
-    out[row][col]=( img[row][col]);
-    255/2=out;
-
-    int &j=j/2;
-    int &k=k/2;
-    for (int col = int&j; int row= int&k;col++){
-      out[j][k];
+void box(std::string input){
+  int j;
+  int k;
+  int img [MAX_H][MAX_W];
+  readImage(input, img, j, k); 
+ 
+  for (int row= j/4; row<=((j/4)*3); row++){
+    for (int col = k/4; col<= (k/4)*3; col++){
+      img[row][col] = 255;
     }
-  
+  }
+    writeImage("taskC.pgm",img, j, k);
 }
 
 //task d frame
 
-void frame(int l,m){
- for (int row=0; row<j;row++){
-    out[row][col]=( img[row][col]);
-   for (int col=m, int row=l);
-     if col ==l/4 || row == m/4;
-     out[l][m]=255;
+void frame(std::string input){
+  int j;
+  int k;
+  int img [MAX_H][MAX_W];
+  readImage(input, img, j, k); 
+ 
+  for (int row= j/4; row<=((j/4)*3);row++){
+    for (int col = k/4; col <= (k/4)*3;col++){
+      if (row == j/4 || row == ((j/4)*3) ){
+        img[row][col] = 255;
+  }
+         else if (col == k/4 || col == ((k/4)*3)){
+      img[row][col] = 255;
+    }
+  }
 }
+
+    writeImage("taskD.pgm",img, j, k);
+    }
